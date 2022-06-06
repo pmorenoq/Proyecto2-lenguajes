ecaae8add36a03746627e341e93edb849b59975b
andres-ochoa-tirado <102266041+andres-ochoa-tirado@users.noreply.github.com>
2022-06-05 20:09:33 -0500

    Update App.java

--- App.java
+++ App.java
@@ -29,7 +29,7 @@ public class App {
     }
 
     public void interfaz(){
-        System.out.println("ingrese la opcion qe desee: ");
+        System.out.println("ingrese la opcion que desee: ");
         System.out.println("(1) para buscar los 3 datos mas relevantes ingresando numeros.");
         System.out.println("(2) para buscar los 5 datos mas relevantes ingresando palabras.");
         System.out.println("(3) para buscar los 5 datos mas relevantes ingresando oraciones.");
